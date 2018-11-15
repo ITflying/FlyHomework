d47ea93c34df9b74ded4901c0c9318f3c7d2a648
ITflying <442665858@qq.com>
2018-11-15 14:11:08 +0800

    [modify]add fastjson dependency and some files

---
+++ flyleetcode/src/main/java/com/hiabby/flyleetcode/base/hello.java
@@ -0,0 +1,15 @@
+package com.hiabby.flyleetcode.base;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * @desc
+ * @date 2018/10/25
+ **/
+public class hello {
+    public static void test(){
+
+    }
+}
---
+++ flyleetcode/src/main/java/com/hiabby/flyleetcode/problems/OneToSixteen.java
@@ -0,0 +1,8 @@
+package com.hiabby.flyleetcode.problems;
+
+/**
+ * @desc
+ * @date 2018/10/23
+ **/
+public class OneToSixteen {
+}
